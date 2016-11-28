ff171e26c70f7da3dc3a76f4c28d985681a8ecb6
Michael S. Mikowski <mike.mikowski@gmail.com>
2016-11-28 12:18:38 -0800

    Updated to show multiple apps.

---
+++ js/ex02.js
@@ -0,0 +1,27 @@
+/**
+ *    ex02.js
+ *    Example 02 applicaton using xhi instances instead of modules.
+ *
+ *    Michael S. Mikowski - mike.mikowski@gmail.com
+*/
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true, todo    : true, unparam  : true
+*/
+/*global $, xhi:true, ex01 */
+$(function () {
+  'use strict';
+  var aMap = xhi._makeApp_( 'ex02' );
+  aMap._shell_._initModule_( $('body') );
+  aMap._lb_._showLb_({
+    _title_html_ : 'Example Application',
+    _content_html_ :
+      '<p><strong>hi_score</strong> appears to be properly installed!</p>'
+      + '<p>The hi_score application map (aMap) attributes are as follows: <br>'
+      + Object.keys( aMap ).join('<br>')
+      + '</p>'
+      + '<p>Grab this lightbox by the title bar to drag</p>'
+  });
+});
